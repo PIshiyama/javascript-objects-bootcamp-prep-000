631dbd1140bb1648d3d8bac9314accf7ad828310
Paul Ishiyama <paul.h.ishiyama@gmail.com>
2018-08-15 03:22:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,5 @@ var playlist = {'Artist': 'SongName'};
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle} );
 }
+
+updatePlaylist()
